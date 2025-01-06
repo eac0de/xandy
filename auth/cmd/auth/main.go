e2c04371b04241a976eeeb30a6a4fadad046a679
egorzh <egor2001zhukov@gmail.com>
2025-01-06 22:43:02 +0300

    fix shared

---
+++ auth/cmd/auth/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/eac0de/xandy/auth/internal/api/handlers"
+	"github.com/eac0de/xandy/auth/internal/api/inmiddlewares"
+	"github.com/eac0de/xandy/auth/internal/config"
+	"github.com/eac0de/xandy/auth/internal/grpcserver"
+	"github.com/eac0de/xandy/auth/internal/services"
+	"github.com/eac0de/xandy/auth/internal/storage"
+	"github.com/eac0de/xandy/shared/pkg/emailsender"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter(
+	sessionService *services.SessionService,
+	authService *services.AuthService,
+) *gin.Engine {
+	router := gin.Default()
+	rootGroup := router.Group("api/auth")
+
+	authHandlers := handlers.NewAuthHandlers(
+		authService,
+		sessionService,
+	)
+
+	rootGroup.POST("code/generate/", authHandlers.GenerateEmailCodeHandler)
+	rootGroup.POST("code/verify/", authHandlers.NewVerifyEmailCodeHandler("/api/auth/token/"))
+	rootGroup.POST("token/", authHandlers.NewRefreshTokenHandler("/api/auth/token/"))
+	rootGroup.DELETE("token/", authHandlers.NewDeleteCurrentSession("/api/auth/token/"))
+
+	authenticatedGroup := rootGroup.Group("/", inmiddlewares.NewAuthMiddleware(sessionService))
+	authenticatedGroup.GET("/sessions/", authHandlers.GetUserSessionsHandler)
+	authenticatedGroup.DELETE("/sessions/:id/", authHandlers.DeleteSession)
+	return router
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg := config.MustLoad()
+
+	if !cfg.IsDev {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	authStorage, err := storage.NewAuthStorage(
+		ctx,
+		cfg.PSQLHost,
+		cfg.PSQLPort,
+		cfg.PSQLUsername,
+		cfg.PSQLPassword,
+		cfg.PSQLDBName,
+	)
+	if err != nil {
+		panic(err)
+	}
+	err = authStorage.Migrate(ctx, "./migrations", false)
+	if err != nil {
+		panic(err)
+	}
+	defer authStorage.Close()
+
+	var emailSender emailsender.IEmailSender
+	if cfg.IsDev {
+		emailSender = emailsender.NewMock()
+	} else {
+		emailSender = emailsender.New(cfg.SMTPHost, cfg.SMTPPort, cfg.SMTPUsername, cfg.SMTPPassword)
+	}
+
+	sessionService := services.NewSessionService(cfg.JWTSecretKey, cfg.JWTAccessExp, cfg.JWTRefreshExp, authStorage)
+	authService := services.NewAuthService(authStorage, emailSender)
+
+	gprcAuthServer := grpcserver.NewAuthGRPCServer(cfg.GPRCServerAddress, sessionService)
+	go gprcAuthServer.Run()
+
+	r := setupRouter(sessionService, authService)
+	go r.Run(cfg.ServerAddress)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	<-sigChan
+}
--- shared/go.mod
+++ shared/go.mod
@@ -3,6 +3,7 @@ module github.com/eac0de/xandy/shared
 go 1.23.3
 
 require (
+	github.com/go-resty/resty/v2 v2.16.2
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/pressly/goose/v3 v3.23.0
 )
@@ -15,6 +16,7 @@ require (
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 )
